5be893cb5033b36bdc4cca44d88df20a759b3b05
jojomo96 <jmoritz@student.42heilbronn.de>
2024-04-01 11:16:22 +0200

    fix negative zoom

--- events.c
+++ events.c
@@ -6,13 +6,11 @@ void	handle_ESC_key(t_data *data)
 	exit(0);
 }
 
-void move_window(t_data *data, int x_offset, int y_offset, double zoom)
+void	move_window(t_data *data, int x_offset, int y_offset)
 {
-  data->y_offset+=y_offset;
-  data->x_offset+=x_offset;
-  data->zoom+=zoom;
-  printf("zoom: %f", data->zoom);
-  put_image(*data);
+	data->y_offset += y_offset;
+	data->x_offset += x_offset;
+	put_image(*data);
 }
 
 int	key_press(int keycode, t_data *data)
@@ -21,16 +19,22 @@ int	key_press(int keycode, t_data *data)
 	if (keycode == ESC_KEY_MAC || keycode == ESC_KEY_LINUX)
 		handle_ESC_key(data);
 	else if (keycode == UP_KEY_MAC || keycode == UP_KEY_LINUX)
-    move_window(data, 0, -OFFSET_PER_CLICK, 0.0);
+		move_window(data, 0, -OFFSET_PER_CLICK);
 	else if (keycode == DOWN_KEY_MAC || keycode == DOWN_KEY_LINUX)
-    move_window(data, 0, OFFSET_PER_CLICK, 0.0);
+		move_window(data, 0, OFFSET_PER_CLICK);
 	else if (keycode == LEFT_KEY_MAC || keycode == LEFT_KEY_LINUX)
-    move_window(data, -OFFSET_PER_CLICK, 0, 0.0);
+		move_window(data, -OFFSET_PER_CLICK, 0);
 	else if (keycode == RIGHT_KEY_MAC || keycode == RIGHT_KEY_LINUX)
-    move_window(data, OFFSET_PER_CLICK, 0, 0.0);
-  else if (keycode == PLUS_KEY)
-    move_window(data, 0,0, -0.1);
-  else if (keycode == MINUS_KEY)
-    move_window(data, 0, 0, 0.1);
+		move_window(data, OFFSET_PER_CLICK, 0);
+	else if (keycode == PLUS_KEY)
+	{
+		data->zoom *= 0.9;
+		move_window(data, 0, 0);
+	}
+	else if (keycode == MINUS_KEY)
+  {
+    data->zoom /= 0.9;
+		move_window(data, 0, 0);
+  }
 	return (0);
 }
