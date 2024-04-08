c4dfcef0c250c56f11f6a71eac01abc005e8b244
Johannes Moritz <jmoritz@2-A-3.42heilbronn.de>
2024-04-08 21:38:54 +0200

    remove dublicate functions

--- utils/complex.c
+++ utils/complex.c
@@ -6,7 +6,7 @@
 /*   By: jmoritz < jmoritz@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 14:15:57 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/04/08 21:33:27 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/04/08 21:38:21 by jmoritz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,42 +77,6 @@ double	complex_abs(t_complex z)
  * @param z The complex number.
  * @return The magnitude of the complex number.
  */
-double	complex_magnitude(t_complex z)
-{
-	return (sqrt(z.re * z.re + z.im * z.im));
-}
-t_complex	complex_add(t_complex a, t_complex b)
-{
-	t_complex	result;
-
-	result.re = a.re + b.re;
-	result.im = a.im + b.im;
-	return (result);
-}
-
-t_complex	complex_sub(t_complex a, t_complex b)
-{
-	t_complex	result;
-
-	result.re = a.re - b.re;
-	result.im = a.im - b.im;
-	return (result);
-}
-
-t_complex	complex_mul(t_complex a, t_complex b)
-{
-	t_complex	result;
-
-	result.re = a.re * b.re - a.im * b.im;
-	result.im = a.re * b.im + a.im * b.re;
-	return (result);
-}
-
-double	complex_abs(t_complex z)
-{
-	return (sqrt(z.re * z.re + z.im * z.im));
-}
-
 double	complex_magnitude(t_complex z)
 {
 	return (sqrt(z.re * z.re + z.im * z.im));
