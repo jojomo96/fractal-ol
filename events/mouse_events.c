414d8c115a16b61f0f01f8d9bb2b1a57d5db2cd7
Johannes Moritz <jmoritz@2-A-3.42heilbronn.de>
2024-04-08 14:43:27 +0200

    Add mouse event handling functions

--- events/mouse_events.c
+++ events/mouse_events.c
@@ -6,53 +6,63 @@
 /*   By: jmoritz < jmoritz@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 11:59:41 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/04/08 14:35:06 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/04/08 14:42:24 by jmoritz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../main.h"
 
+static void	handle_julia_c(t_data *data, double x, double y)
+{
+	const double	threshold = 0.03;
+	double			delta_x;
+	double			delta_y;
+
+	delta_x = fabs((x / data->mlx->width * 4 - 2)
+			- get_config_value(JULIA_C_REAL));
+	delta_y = fabs((y / data->mlx->height * 4 - 2)
+			- get_config_value(JULIA_C_IMAGINARY));
+	if (delta_x > threshold || delta_y > threshold)
+	{
+		set_config_value(JULIA_C_REAL, x / data->mlx->width * 4 - 2);
+		set_config_value(JULIA_C_IMAGINARY, y / data->mlx->height * 4 - 2);
+		put_image(*data);
+	}
+}
+
+static void	handle_dragging(t_data *data, t_point pos_old)
+{
+	double	delta_x;
+	double	delta_y;
+	double	move_sensitivity;
+
+	move_sensitivity = 1.0;
+	delta_x = pos_old.x - data->mouse_x;
+	delta_y = pos_old.y - data->mouse_y;
+	delta_x *= move_sensitivity;
+	delta_y *= move_sensitivity;
+	if ((fabs(delta_x) > 1 && fabs(delta_x) < 50) && (fabs(delta_y) > 1
+			&& fabs(delta_y) < 50))
+	{
+		move_window(data, delta_x, delta_y);
+	}
+}
+
 void	mouse_move_event(double x, double y, void *param)
 {
 	t_data			*data;
-	const double	thresholdForJulia = 0.03;
 	t_point			pos_old;
-	double			deltaX;
-	double			deltaY;
-	double			moveSensitivity;
 
-	moveSensitivity = 1.0;
 	data = (t_data *)param;
 	pos_old.x = data->mouse_x;
 	pos_old.y = data->mouse_y;
 	data->mouse_x = x;
 	data->mouse_y = y;
 	if (mlx_is_key_down(data->mlx, MLX_KEY_LEFT_SHIFT))
-	{
-		deltaX = fabs((x / data->mlx->width * 4 - 2)
-				- get_config_value(JULIA_C_REAL));
-		deltaY = fabs((y / data->mlx->height * 4 - 2)
-				- get_config_value(JULIA_C_IMAGINARY));
-		if (deltaX > thresholdForJulia || deltaY > thresholdForJulia)
-		{
-			set_config_value(JULIA_C_REAL, x / data->mlx->width * 4 - 2);
-			set_config_value(JULIA_C_IMAGINARY, y / data->mlx->height * 4 - 2);
-			put_image(*data);
-		}
-	}
+		handle_julia_c(data, x, y);
 	if (get_flag(IS_DRAGGING) && !mlx_is_key_down(data->mlx,
 			MLX_KEY_LEFT_SHIFT))
-	{
-		deltaX = pos_old.x - data->mouse_x;
-		deltaY = pos_old.y - data->mouse_y;
-		deltaX *= moveSensitivity;
-		deltaY *= moveSensitivity;
-		if ((fabs(deltaX) > 1 && fabs(deltaX) < 50) && (fabs(deltaY) > 1
-				&& fabs(deltaY) < 50))
-		{
-			move_window(data, deltaX, deltaY);
-		}
-	}
+		handle_dragging(data, pos_old);
 }
 
 void	mouse_klick_event(mouse_key_t button, action_t action,
